350f0e07b548c13c6ecc5bdcff4e39f52f75a6d5
Marcelo Cavalcante <marcelo.cavalcante@ebac.art.br>
2022-03-15 21:40:03 -0300

    code together portfolio

--- single-portfolio.php
+++ single-portfolio.php
@@ -2,13 +2,26 @@
 
 <main class="container">
 
+    <section>
+        <div class="container">
+            <h2> <?php the_title() ?> </h2>
+            <?php the_content() ?>
+            <?php 
+                if(has_post_thumbnail()){
+                    the_post_thumbnail('imagem_horizontal');
+                }
+            ?>
+        </div>
+    </section>
+
     <aside>
         <?php 
 
             $args = array(
                 'post_type' => 'portfolio',
                 'posts_per_page' => '5',
-                'orderby' => 'title'
+                'orderby' => 'title',
+                'post__not_in' => [get_the_ID()]
             );
 
             $portfolioItens = new WP_Query($args);
@@ -16,7 +29,6 @@
             while( $portfolioItens->have_posts()) : 
                 $portfolioItens->the_post();
             
-        
         ?>
             <div class="portfolio-item">
                 <h3><?php the_title(); ?></h3>
@@ -24,22 +36,14 @@
                 <a href="<?php the_permalink() ?>">
                 <?php 
                 if(has_post_thumbnail()){
-                    the_post_thumbnail('imagem_horizontal');
+                    the_post_thumbnail();
                 }
                 ?>
                 </a>
-                <p><a href="<?php the_permalink() ?>">Saiba mais</a>
-            </p>
+                <p><a href="<?php the_permalink() ?>">Saiba mais</a></p>
             </div>
         <?php endwhile; ?>
     </aside>
 
-    <section>
-        <div class="container">
-            <h2> <?php the_title() ?> </h2>
-            <?php the_content() ?>
-        </div>
-    </section>
-    
 </main>
 <?php get_footer(); ?>
